dd33afc52a4cdc21176e9e6c468d25280f276bff
Michael Kuan <michael34435@capslock.tw>
2022-08-19 21:39:14 +0800

    first commit

---
+++ app.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"flag"
+	"os"
+
+	"github.com/gin-gonic/gin"
+	"github.com/michael34435/lnurlp/models"
+
+	"github.com/go-pg/pg/v10"
+	"github.com/go-pg/pg/v10/orm"
+)
+
+func createSchema(db *pg.DB) error {
+	models := []interface{}{
+		(*models.Alias)(nil),
+	}
+
+	for _, model := range models {
+		err := db.Model(model).CreateTable(&orm.CreateTableOptions{
+			IfNotExists: true,
+		})
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func main() {
+	db := pg.Connect(&pg.Options{
+		User:     os.Getenv("PG_USER"),
+		Password: os.Getenv("PG_PASS"),
+		Database: os.Getenv("PG_DB"),
+		Addr:     os.Getenv("PG_HOST"),
+	})
+	defer db.Close()
+
+	err := createSchema(db)
+	if err != nil {
+		panic(err)
+	}
+
+	prod := flag.Bool("prod", false, "for production")
+	flag.Parse()
+
+	if *prod {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	r.Use(gin.ErrorLogger())
+	r.GET("/.well-known/lnurlp/:key", func(c *gin.Context) {
+		key := c.GetString("key")
+
+		alias := models.Alias{Key: key}
+		err := db.Model(&alias).Select()
+
+		if err != nil {
+			c.AbortWithError(400, err)
+			return
+		}
+
+		c.Redirect(301, "https://"+alias.Domain+"/.well-known/lnurlp/"+alias.User)
+	})
+	r.Run()
+}
---
+++ models/alias.go
@@ -0,0 +1,8 @@
+package models
+
+type Alias struct {
+	ID     int64
+	Key    string
+	User   string
+	Domain string
+}
